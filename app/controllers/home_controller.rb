ccb1e9cb47a2a616f4de959e4291bc34b231b97b
aoringo0718 <tsuruta0718@outlook.jp>
2022-12-22 21:08:53 +0900

    maintenance of environment

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+class HomeController < ApplicationController
+    def top
+    end
+    def nintendo
+    end
+    def newyork
+    end
+    def jurassic
+    end
+    def hollywood
+    end
+    def harrypotter
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/" => "home#top"
+  get "nintendo" => "home#nintendo"
+  get "newyork" => "home#newyork"
+  get "hollywood" => "home#hollywood"
+  get "jurassic" => "home#jurassic"
+  get "harrypotter" => "home#harrypotter"
 end
